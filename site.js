085d33e73b151b5db7652e2195bd6b19e912fd9f
Corbin Brooke <corbinng@gmail.com>
2018-11-20 09:22:06 -0600

    first commit

---
+++ site.js
@@ -0,0 +1,11 @@
+// $('#circle').click(function() {
+// 	alert('Clicked!');
+// });
+
+// $('#circle').hover(function() {
+// 	$('img').attr('src', 'https://chainimage.com/images/football-field-with-football-for-desktop-14-hd-wallpapers.jpg');
+// });
+
+$(.square).hover(function() {
+	$(this).css('width', '400px');
+})
